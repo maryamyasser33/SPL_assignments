7a5d0a7862b0263984a9753e593eea12cb5dd8d2
maryamyasser33 <168692130+maryamyasser33@users.noreply.github.com>
2025-04-09 19:40:08 +0200

    Update cp.c

--- cp.c
+++ cp.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+int cp_main(int argc, char *argv[]) {
+    FILE  *fptr1,*fptr2;
+    fptr1 = fopen(argv[1], "r");
+
+     if (fptr1 == NULL ) {
+        printf("error has occured");
+        return -1;
+    }
+    fptr2 = fopen(argv[2], "w");
+    if (fptr2 == NULL ) {
+        printf("error has occured");
+        return -1;
+    }
+    char c = fgetc(fptr1);
+    while(c!=EOF){fputc(c,fptr2);c = fgetc(fptr1);}
+    fclose(fptr1);
+    fclose(fptr2);
+    return 0;
+
+}
